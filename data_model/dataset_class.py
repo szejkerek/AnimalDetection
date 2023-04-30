Fix normalization bug

data_model/dataset_class.py
@@ -64,10 +64,10 @@ class Dataset(BaseDataset):
         for cls_value in self.class_values:
             color = config.COLORS[cls_value]
             newMask = cv2.inRange(mask, color, color)
+            newMask = np.float32(newMask)
+            newMask = cv2.normalize(newMask, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
             masks.append(newMask)
-
-        mask = np.stack(masks, axis=-1).astype('float')
-
+        mask = np.stack(masks, axis=-1).astype('float32')
         # apply augmentations
         if self.augmentation:
             sample = self.augmentation(image=image, mask=mask)

utils/visualizers.py
@@ -141,10 +141,10 @@ def save_results(test_visualize, test_dataset, count=10):
 
         # Convert images and masks to PTL images
         normal_image = Image.fromarray(np.uint8(image_vis))
-        gt_animals = Image.fromarray(np.uint8(gt_mask[0, ...].squeeze())).convert('RGB')
-        gt_masking_background = Image.fromarray(np.uint8(gt_mask[1, ...].squeeze())).convert('RGB')
-        gt_nonmasking_background = Image.fromarray(np.uint8(gt_mask[2, ...].squeeze())).convert('RGB')
-        gt_foreground_attention = Image.fromarray(np.uint8(gt_mask[3, ...].squeeze())).convert('RGB')
+        gt_animals = Image.fromarray(np.uint8(gt_mask[0, ...].squeeze() * 255)).convert('RGB')
+        gt_masking_background = Image.fromarray(np.uint8(gt_mask[1, ...].squeeze() * 255)).convert('RGB')
+        gt_nonmasking_background = Image.fromarray(np.uint8(gt_mask[2, ...].squeeze() * 255)).convert('RGB')
+        gt_foreground_attention = Image.fromarray(np.uint8(gt_mask[3, ...].squeeze() * 255)).convert('RGB')
 
         pr_animals = Image.fromarray(np.uint8(pr_mask[0, ...].squeeze() * 255)).convert('RGB')
         pr_masking_background = Image.fromarray(np.uint8(pr_mask[1, ...].squeeze() * 255)).convert('RGB')